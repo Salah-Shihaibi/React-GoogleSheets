618d8cd4a395c10a197236948cc4cba479e32064
salah <salah.shihaibi0@gmail.com>
2021-10-19 20:13:56 +0100

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 //import { useState, useEffect, Component, useContext } from "react";
 import Error404 from "./Pages/404";
 import Home from "./Pages/Home";
@@ -9,13 +9,12 @@ import About from "./Pages/About";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} />
-        <Route path="/*" component={Error404} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
